a0428536deeccfd54287f169d63a3e85018e36d6
creattech <crystalsmcneil@gmail.com>
2020-11-15 15:57:10 -0600

    adds rest framework auth token

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title', 'description']
+        fields = ['id','title', 'description', 'price', 'published', 'is_published', 'cover']
