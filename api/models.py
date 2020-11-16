083bf394547e0a01c02a5be844d7d3e167ea6687
creattech <crystalsmcneil@gmail.com>
2020-11-15 19:03:30 -0600

    adds booknumber onetoone model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class BookNumber(models.Model):
+    isbn_10 = models.CharField(max_length=10, blank=True)
+    isbn_13 = models.CharField(max_length=13, blank=True)
+
 class Book(models.Model):
     title = models.CharField(max_length=36, blank=False, unique=True)
     description = models.TextField(max_length=256, blank=True)
@@ -7,6 +11,8 @@ class Book(models.Model):
     published = models.DateField(blank=True, null=True, default=None)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='covers/', blank=True)
+    number = models.OneToOneField(BookNumber, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
